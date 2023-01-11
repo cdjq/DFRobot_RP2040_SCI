b62adee5ed86d90e4bab0fa99f9eae9c471f5459
Frances9 <44355133+Frances9@users.noreply.github.com>
2023-01-11 09:54:45 +0800

    Update demo_config.py

--- python/raspberrypi/examples/demo_config.py
+++ python/raspberrypi/examples/demo_config.py
@@ -38,9 +38,9 @@ if __name__ == "__main__":
   print("Address: 0x%x"%addr)
   
   '''!
-    @brief Set the I2C communication address of SCI Acquisition Module 
-    @param addr I2C communication address of SCI Acquisition Module, support the following address settings
-    @n RP2040_SCI_ADDR_0X21      0x21 Default I2C address 
+    @brief Set the I2C address of SCI Acquisition Module 
+    @param addr I2C address of SCI Acquisition Module, support the following address settings
+    @n RP2040_SCI_ADDR_0X21      0x21 default I2C address 
     @n RP2040_SCI_ADDR_0X22      0x22
     @n RP2040_SCI_ADDR_0X23      0x23
     @return Error code
