70a05188df41344596467532a04276314aa5c8be
Frances9 <44355133+Frances9@users.noreply.github.com>
2023-01-14 16:19:22 +0800

    Update valueUnit.ino

--- examples/valueUnit/valueUnit.ino
+++ examples/valueUnit/valueUnit.ino
@@ -1,6 +1,6 @@
 /*!
  * @file valueUnit.ino
- * @brief According to the name of SEN0334 temperature and humidity sensor data, get the corresponding value and unit
+ * @brief According to the data name of SEN0334 temperature and humidity sensor, get the corresponding value and unit
  *
  * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  * @license     The MIT License (MIT)
